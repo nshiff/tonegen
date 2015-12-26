97ce0662b232206bf3b2a1b8ef004b996d67b34e
Nate S <nshiff@users.noreply.github.com>
2015-12-25 20:44:22 -0500

    refactor to use only pin 8 for audio out

--- tonegen.ino
+++ tonegen.ino
@@ -1,19 +1,4 @@
-/*
-  Mega analogWrite() test
- 	
-  This sketch fades LEDs up and down one at a time on digital pins 2 through 13.  
-  This sketch was written for the Arduino Mega, and will not work on previous boards.
- 	
-  The circuit:
-  * LEDs attached from pins 2 through 13 to ground.
 
-  created 8 Feb 2009
-  by Tom Igoe
-  
-  This example code is in the public domain.
-  
- */
- 
  
  #ifndef _Waveforms_h_
 #define _Waveforms_h_
@@ -98,59 +83,40 @@ static int waveformsTable[maxWaveform][maxSamplesNum] = {
  
  
  
-// These constants won't change.  They're used to give names
-// to the pins used:
-const int lowestPin = 4;
-const int highestPin = 8;
 
+int pinAudioOut = 8;
 
 void setup() {
-  // set pins 2 through 13 as outputs:
-  for (int thisPin =lowestPin; thisPin <= highestPin; thisPin++) { 
-    pinMode(thisPin, OUTPUT); 
-  }
+  pinMode(pinAudioOut, OUTPUT);
+  
 }
 
 void loop() {
-  // iterate over the pins:
-  for (int thisPin =lowestPin; thisPin <= highestPin; thisPin++) { 
-    // fade the LED on thisPin from off to brightest:
-    for (int brightness = 0; brightness < 255; brightness++) {
-      analogWrite(thisPin, brightness);
-      delay(2);
-    } 
-    // fade the LED on thisPin from brithstest to off:
-    for (int brightness = 255; brightness >= 0; brightness--) {
-      analogWrite(thisPin, brightness);
-      delay(2);
-    } 
-    // pause between LEDs:
-    delay(100);
-    
+  
     int duration = 500;
-    if(thisPin == 8){  //speaker plugged into pin 8
+
       for(int wavelengths=0; wavelengths<duration; wavelengths++){
         for(int i=0; i<120; i++){ //array is 120 elements long
-          analogWrite(thisPin, waveformsTable[0][i]);
+          analogWrite(pinAudioOut, waveformsTable[0][i]);
         }
       }
       delay(100);
       for(int wavelengths=0; wavelengths<duration; wavelengths++){
         for(int i=0; i<120; i++){ //array is 120 elements long
-          analogWrite(thisPin, waveformsTable[1][i]);
+          analogWrite(pinAudioOut, waveformsTable[1][i]);
         }
       }
 
       delay(100);
       for(int wavelengths=0; wavelengths<duration; wavelengths++){
         for(int i=0; i<120; i++){ //array is 120 elements long
-          analogWrite(thisPin, waveformsTable[2][i]);
+          analogWrite(pinAudioOut, waveformsTable[2][i]);
         }
       }      
       
       
-    }
     
+    delay(800);
     
-  }
+  
 }
